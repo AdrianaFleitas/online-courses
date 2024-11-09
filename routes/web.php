050ce24d92f999f12ba3e9f9ab1ace15befeb53a
apaezdelima <apaezdelima@gmail.com>
2024-11-09 12:42:26 -0300

    git init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('dashboard', [UserController::class, 'dashboard']);
+Route::get('users', [UserController::class, 'users']);
